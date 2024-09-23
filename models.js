6c267ca5a3b19b23a9faab6e2391671c70539ebf
Julita23 <julibibiloni67@gmail.com>
2024-09-22 22:28:14 -0300

    fix try/catch

--- models.js
+++ models.js
@@ -4,9 +4,13 @@ import { randomUUID } from "node:crypto";
 // Averiguar como "activar" la lectura de las variables de entorno del archivo .env (dotenv)
 import { handleError } from "./utils/handleError.js";
 import { get } from "node:http";
+import { config } from "dotenv";
+
+config();
 
 // 1° recuperar variables de entorno
-const PATH_FILE = process.env.PATH_FILE;
+const PATH_USERS_FILE = process.env.PATH_USERS_FILE;
+const PATH_USERS_ERROR = process.env.PATH_USERS_ERROR;
 // 2° Declarar los metodos
 
 const getUsers = (urlFile) => {
@@ -16,18 +20,18 @@ const getUsers = (urlFile) => {
       throw new Error("Don't have permissions")
     }
 
-    const existsFile = existsSync(PATH_FILE);
+    const existsFile = existsSync(PATH_USERS_FILE);
 
     if(!existsFile) {
-      writeFileSync(PATH_FILE, JSON.stringify([]));
+      writeFileSync(PATH_USERS_FILE, JSON.stringify([]));
       throw new Error("File doesn't exists...")
     }
 
-    const users = JSON.parse(readFileSync(PATH_FILE));
+    const users = JSON.parse(readFileSync(PATH_USERS_FILE));
     return users;
   
   } catch (error) {
-     const objError = handleError()
+     const objError = handleError(error, PATH_USERS_ERROR)
      return objError;
   }
 };
@@ -37,8 +41,26 @@ console.log(response);
 
 const getUserById = (id) => {
   try {
-  } catch (error) { }
+    if(!id) {
+      throw new Error("ID is missing");
+    }
+
+    const users = getUsers(PATH_USERS_FILE);
+    const user = users.find((user) => movie.id === id);
+
+    if(!user) {
+      throw new Error("User not found");
+    }
+    
+    return user;
+
+  } catch (error) {
+    const objError = handleError(error, PATH_USERS_ERROR)
+    return objError;
+   }
 };
+
+console.log(getUserById(12345));
 
 // addUser recibe un objeto con toda la data para el nuevo usuario
 // valida que esten los datos míminos para añadir un nuevo usuario
@@ -47,78 +69,88 @@ const getUserById = (id) => {
 // valida que el email sea un string y que no se repita
 // hashea la contraseña antes de registrar al usuario
 const addUser = (userData) => {
-  const { nombre, apellido, email, password } = userData;
-
-  const users = getUsers();
-  const existsUser = users.find(userData => userData.nombre === nombre);
-
-  if (existsUser) {
-    return "User already exists"
-  }
-
-  const newUser = {
-    id: randomUUID(),
-    nombre,
-    apellido,
-    email,
-    password,
-    isLoggedId,
-  };
-
-  users.push(newUser);
-  writeFileSync(PATH_FILE, JSON.stringify(users));
-
-  return newUser;
-
   try {
-  } catch (error) { }
+    const { nombre, apellido, email, password } = userData;
+
+    const existsUser = users.find(userData => userData.nombre === nombre);
+  
+    if (existsUser) {
+      throw new Error ("User already exists");
+    }
+
+    if(!nombre || !apellido || !email || !password) {
+      throw new Error ("Missing data");
+    }
+  
+    const newUser = {
+      id: randomUUID(),
+      nombre,
+      apellido,
+      email,
+      password,
+      isLoggedId,
+    };
+  
+    const users = getUsers(PATH_USERS_FILE);
+    users.push(newUser);
+    writeFileSync(PATH_USERS_FILE, JSON.stringify(users));
+  
+    return newUser;
+
+  } catch (error) { 
+    const objError = handleError(error, PATH_USERS_ERROR)
+    return objError;
+  }
 };
 
 // todos los datos del usuario seleccionado se podrían modificar menos el ID
 // si se modifica la pass debería ser nuevamente hasheada
 // si se modifica el email, validar que este no exista
 const updateUser = (id, userData) => {
-  const { nombre, apellido, email, password } = userData;
-
-  const users = getUsers();
-
-  const existsUser = users.find(userData => userData.id === id);
-
-  if (!existsUser) {
-    return "User doesn't exists";
-  }
-
-  if (nombre) existsUser.nombre = nombre;
-  if (apellido) existsUser.apellido = apellido;
-  if (email) existsUser.email = email;
-  if (password) existsUser.password = password;
-
-  writeFileSync(PATH_FILE, JSON.stringify(users));
-
-  return existsUser;
-
   try {
-  } catch (error) { }
+    const { nombre, apellido, email, password } = userData;
+
+    if(!id || !userData) {
+      throw new Error("ID is missing");
+    }
+  
+    const users = getUsers(PATH_USERS_FILE);
+    const user = getUserById(id);
+  
+    if (nombre) user.nombre = nombre;
+    if (apellido) user.apellido = apellido;
+    if (email) user.email = email;
+    if (password) user.password = password;
+  
+    writeFileSync(PATH_FILE, JSON.stringify(users));
+  
+    return user;
+  
+  } catch (error) { 
+    const objError = handleError(error, PATH_USERS_ERROR)
+    return objError;
+  }
 };
 
 const deleteUser = (id) => {
-
-  const users = getUsers();
-
-  const existsUser = users.find(userData => userData.id === id);
-
-  if (!existsUser) {
-    return "Not found user";
-  }
+  try {
+    if(!id) {
+      throw new Error("ID is missing");
+    }
+  
+    const users = getUsers(PATH_USERS_FILE);
+    const user = getUserById(id);
 
   const newUser = users.filter((user) => user.id !== id);
 
   writeFileSync(PATH_FILE, JSON.stringify(newUser));
 
-  return existsUser;
+  return user;
 
-  try {
-  } catch (error) { }
+  } catch (error) { 
+    const objError = handleError(error, PATH_USERS_ERROR)
+    return objError;
+  }
 };
 
 export { getUsers, getUserById, addUser, updateUser, deleteUser };
